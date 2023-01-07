c401f3fa0e8a018acf0eb927a21617c64dad2033
Jawwaad <jawwaad.sabree01@gmail.com>
2023-01-07 16:58:47 -0500

    feat: added history page functionality

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -1,6 +1,6 @@
+import 'package:fantasy_fitness/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:health/health.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
@@ -12,9 +12,47 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, y) {
-        return ListTile(title: Text(y.toString()));
+    return FutureBuilder<List>(
+      future: supabase
+          .from('fit_data')
+          .select<List>()
+          .eq('user_id', currUser?.id ?? ''),
+      builder: (context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasError) {
+          Future.delayed(const Duration(milliseconds: 100), () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Error getting Data'),
+              ),
+            );
+          });
+          return Container();
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else {
+          // List<HealthDataPoint> data = snapshot.data as List<HealthDataPoint>;
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            prototypeItem: ListTile(
+              title: Text(
+                snapshot.data!.first['data_type'],
+              ),
+              subtitle: Text(
+                snapshot.data!.first['value'].toString(),
+              ),
+            ),
+            itemBuilder: (BuildContext context, i) {
+              return ListTile(
+                title: Text(
+                  snapshot.data![i]['data_type'],
+                ),
+                subtitle: Text(
+                  snapshot.data![i]['value'].toString(),
+                ),
+              );
+            },
+          );
+        }
       },
     );
   }
